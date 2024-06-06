9e1d68a0b0b9f7ac36a44e109f02ae266f2bc6e2
azartechnet <azartechnet@gmail.com>
2024-06-06 18:57:47 +0530

    updated

--- Reactjs/routerapp/src/pages/Home.js
+++ Reactjs/routerapp/src/pages/Home.js
@@ -1,5 +1,30 @@
+import { b1 } from './images/b1.jpeg';
 const Home=()=>
 {
-    return<h1>Home Page</h1>
+    return(
+        <>
+        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="" class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src={b1} class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="..."/>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+        </>
+    )
 }
 export default Home;
--- Reactjs/routerapp/src/pages/Layout.js
+++ Reactjs/routerapp/src/pages/Layout.js
@@ -2,21 +2,40 @@ import {Outlet,Link} from 'react-router-dom'
 
 const Layout=()=>{
     return(
-        <>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/Contact">Contact</Link>
-          </li>
-          <li>
-            <Link to="/Blogs">Blogs</Link>
-          </li>
-          <li>
-            <Link to="/NoPage">NoPage</Link>
-          </li>
-          <Outlet/>
-        </>
+      <>
+       <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          
+          <Link to="/" class="nav-link active" aria-current="page">Home</Link>
+        </li>
+        <li class="nav-item">
+          
+          <Link to="/Contact" class="nav-link">Contact</Link>
+
+        </li>
+        <li class="nav-item">
+          
+          <Link to="/Blogs" class="nav-link">Blogs</Link>
+        </li>
+        <li class="nav-item">
+          
+          <Link to="/NoPage" class="nav-link">NoPage</Link>
+        </li>
+        
+      </ul>
+    </div>
+  </div>
+  
+</nav>
+<Outlet/>
+</>
     )
 }
 export default Layout;
